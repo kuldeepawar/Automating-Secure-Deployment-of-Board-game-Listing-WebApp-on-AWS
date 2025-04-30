e788eb336186195759f149cfd30beec50298a4e4
kuldeepawar <37316881+kuldeepawar@users.noreply.github.com>
2025-04-30 06:46:48 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "app_server" {
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
-  key_name                    = aws_key_pair.deployer.key_name
+  key_name                    = "deployer" 
   associate_public_ip_address = false # Elastic IP will be attached
 
   tags = {
