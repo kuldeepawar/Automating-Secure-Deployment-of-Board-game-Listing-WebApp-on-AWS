6649fe95387ed005de7d7c9bdc97c033666f5254
Kuldeep Pawar <37316881+kuldeepawar@users.noreply.github.com>
2025-05-12 16:55:34 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -61,9 +61,4 @@ resource "aws_eip_association" "eip_assoc" {
 }
 
 
-resource "aws_eip_association" "eip_assoc" {
-  instance_id   = aws_instance.app_server.id
-  allocation_id = aws_eip.static_eip.id
-}
-
 
