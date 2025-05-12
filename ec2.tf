5ecf07b598a1af4a655ffe9ebda23861244b3085
Kuldeep Pawar <37316881+kuldeepawar@users.noreply.github.com>
2025-05-12 16:42:20 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -65,3 +65,7 @@ resource "aws_eip_association" "eip_assoc" {
   instance_id   = aws_instance.app_server.id
   allocation_id = aws_eip.static_eip.id
 }
+
+output "public_ip" {
+  value = aws_eip.app_eip.public_ip
+}
